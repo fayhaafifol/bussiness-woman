717ebbb860c7db328d819e0569b9c2e5dd6d3c25
fayhaafifol <fayhaaalsakkar@gmail.com>
2023-08-26 04:38:29 +0300

    started on the interface

---
+++ main.py
@@ -0,0 +1,25 @@
+
+
+print("Choose what you want to do (^w^):")
+print("===================================")
+print("1) Create account")
+print("2) Deposit Money")
+print("3) Withdraw Money")
+print("4) transfer Money")
+print("5) See My Balance")
+
+choice=int(input(">"))
+
+class lebank:
+
+
+    def __init__(self):
+
+        database ={}
+
+mybank = lebank()
+
+
+
+
+
